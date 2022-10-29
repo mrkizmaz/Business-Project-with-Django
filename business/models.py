21b61cfbfafce316625901381965749855a6515f
mrkizmaz <ersel.kizmaz@gmail.com>
2022-10-29 16:54:11 +0300

    data added in panel

--- business/models.py
+++ business/models.py
@@ -7,7 +7,7 @@ from django.db import models
 class Business(models.Model):
     publisher = models.ForeignKey("auth.User", on_delete = models.CASCADE, verbose_name = "Publisher")
     company = models.CharField(max_length = 50, verbose_name = "The Company")
-    position = models.CharField(max_length = 50, verbose_name = "Position")
+    position = models.CharField(max_length = 100, verbose_name = "Position")
     qualifications = models.TextField(verbose_name = "Qualifications")
     location = models.CharField(max_length = 50, verbose_name = "Location")
     published_date = models.DateTimeField(auto_now_add = True, verbose_name = "Published Date")
